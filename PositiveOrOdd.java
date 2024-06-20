9dc255ea1efc03166b71023f1d6ced34aa270ad5
chsrkh <chsrkh@gmail.com>
2024-06-20 11:08:03 +0530

    PositiveOrNeg

---
+++ PositiveOrOdd.java
@@ -0,0 +1,26 @@
+package Java;
+import java.util.Scanner;
+public class PositiveOrOdd
+{
+    public static void main(String[] args) 
+    {
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        String y=positiveOrOdd(x);
+        System.out.println(y);
+        sc.close();
+        
+    }
+    public static String positiveOrOdd(int x)
+    {
+        if(x>0)
+        {
+            return "Even";
+        }
+        else
+        {
+            return "Odd";
+        }
+    }
+    
+}
