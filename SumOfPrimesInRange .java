c3fe1627515bcc150d3157fecf1fc80492c1d701
SRIKANTH CHAKATI <145051600+chsrkh@users.noreply.github.com>
2024-06-22 21:28:17 +0530

    SumOfPrimesInRange

---
+++ SumOfPrimesInRange .java
@@ -0,0 +1,43 @@
+// Program to find sum of the primes in the user given range .
+import java.util.Scanner;
+public class SumOfPrimesInRange 
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int sum=sumOfPrimesInRange(a, b);
+        System.out.println(sum);
+        sc.close();
+
+    }
+    public static int sumOfPrimesInRange(int a, int b)
+    {
+        int sum=0;
+        for(int i=a; i<=b; i++)
+        {
+            if(isPrime(i))
+            {
+                sum+=i;
+            }
+        }
+        return sum;
+    }
+    public static boolean isPrime(int n)
+    {
+        if(n==1)
+        {
+            return false;
+        }
+        for(int i=2; i<n; i++)
+        {
+            if(n%i==0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+}
