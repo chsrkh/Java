58ae0335db262da8b70f8e25a7e7b5df1f58efb8
SRIKANTH CHAKATI <145051600+chsrkh@users.noreply.github.com>
2024-06-24 12:18:38 +0530

    Sum Of Digits, Even Digits, Odd Digits, Prime Digits Of Given Num

--- SumOfDigitsOfGivenNum.java
+++ SumOfDigitsOfGivenNum.java
@@ -1,4 +1,5 @@
-// Program to calculate and return the sum of the digits of a user given number.
+// Program to calculate and return the sum of the all digits, even digits and prime digits  of a user given number.
+
 import java.util.Scanner;
 public class SumOfDigitsOfGivenNum 
 {
@@ -6,7 +7,10 @@ public class SumOfDigitsOfGivenNum
     {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
-        System.out.println(sumOfDigitsOfGivenNum(n));
+        System.out.println("The sum of all the digits = "+sumOfDigitsOfGivenNum(n));
+        System.out.println(" The sum of even digits = "+sumOfEvenDigits(n));
+        System.out.println(" The sum of odd digits = "+sumOfOddDigits(n));
+        System.out.println(" The sum of prime digits = "+sumOfPrimeDigits(n));
         sc.close();
 
     }
@@ -19,6 +23,43 @@ public class SumOfDigitsOfGivenNum
             n=n/10;
         }
         return sum;
+    }
+    public static int sumOfEvenDigits(int n)
+    {
+        int sum=0;
+        while(n!=0)
+        {
+            int r=n%10;
+            if (r%2==0)
+            sum=sum+r;
+            n=n/10;
+        }
+        return sum;
+    }
+    public static int sumOfOddDigits(int n)
+    {
+        int sum=0;
+        while(n!=0)
+        {
+            int r=n%10;
+            if (r%2!=0)
+            sum=sum+r;
+            n=n/10;
+        }
+        return sum;
+    }
+    public static int sumOfPrimeDigits(int n)
+    {
+        int sum=0;
+        while(n!=0)
+        {
+            int r=n%10;
+            if (r==2 || r==3 || r==5 || r==7)
+            sum=sum+r;
+            n=n/10;
+        }
+        return sum;
+
     }
     
 }
